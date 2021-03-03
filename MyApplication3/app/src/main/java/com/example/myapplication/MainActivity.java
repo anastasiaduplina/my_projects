72bf0a47b56a4151172fffba7d4145b70959c458
anastasiaduplina <61756179+anastasiaduplina@users.noreply.github.com>
2021-03-03 17:21:10 +0300

    o

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.google.android.material.internal.ViewOverlayImpl;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener,AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         MyView myView= findViewById(R.id.panel);
         RadioGroup shapes = findViewById(R.id.rg2);
-        class Listener implements View.OnClickListener, RadioGroup.OnCheckedChangeListener{
+        Spinner color= findViewById(R.id.colours);
+        class Listener implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
             @Override
             public void onClick(View v) {
@@ -37,11 +41,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         //RadioGroup.OnCheckedChangeListener;
-        Listener listener = new Listener();
 
+        String[] colors= getResources().getStringArray(R.array.coloursnum);
+         class Spinner extends  Activity implements AdapterView.OnItemSelectedListener {
+             @Override
+             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                 myView.color=colors[position];
+                 Log.i( "changeColor",colors[position] + "");
+
+             }
+
+             @Override
+             public void onNothingSelected(AdapterView<?> parent) {
+
+             }
+         }
+        Listener listener = new Listener();
+       Spinner spinner= new Spinner();
         Button undoBtn=findViewById(R.id.undo);
         undoBtn.setOnClickListener(listener);
         shapes.setOnCheckedChangeListener(listener);
+        color.setOnItemSelectedListener(spinner);
     }
 
 
@@ -59,5 +79,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.circle:break;
             case R.id.triangle:break;
         }
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
