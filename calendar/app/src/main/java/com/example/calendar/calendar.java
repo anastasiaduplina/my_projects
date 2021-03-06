3fe8320d79ce471989c401e079ffc08d6edc37aa
anastasiaduplina <61756179+anastasiaduplina@users.noreply.github.com>
2021-03-06 16:15:17 +0300

    https://applandeo.com/blog/material-calendar-view-customized-calendar-widget-android/

---
+++ calendar/app/src/main/java/com/example/calendar/calendar.java
@@ -0,0 +1,16 @@
+package com.example.calendar;
+
+public class calendar {
+    int day;
+    int month;
+    int year;
+    String color;
+    calendar[] mood = new calendar[100];
+ public calendar(int day, int month, int year, String color){
+     this.day= day;
+     this.month= month;
+     this.year=year;
+     this.color=color;
+ }
+
+}
